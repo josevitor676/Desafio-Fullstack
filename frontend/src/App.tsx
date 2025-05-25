432ed94864f3ac245f6b26a0193a1c8f07ac384e
Jose Vitor <zsantos@fatecie.edu.br>
2025-05-24 21:11:57 -0300

    feat: adicionando .env e algumas bibliotecas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import { TabelaDesenvolvedor } from './components/tabela/TabelaDesenvolvedor'
+import { TabelaNivel } from './components/tabela/TabelaNivel'
+import { Tabs, TabsList, TabsTrigger } from './components/ui/tabs'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)} className='text-red-400'>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="w-screen h-screen p-2">
+      <p className='mb-5'>TESTE GAZIN TECH</p>
+      <Tabs defaultValue="niveis" className="">
+        <TabsList className="grid w-full grid-cols-2">
+        <TabsTrigger value="niveis" className='hover:border-none border-none p-2'>Niveis</TabsTrigger>
+        <TabsTrigger value="desenvolvedores" className='hover:border-none border-none p-2'>Desenvolvedores</TabsTrigger>
+      </TabsList>
+      <TabelaNivel />
+      <TabelaDesenvolvedor />
+      </Tabs>
+    </div>
   )
 }
 
